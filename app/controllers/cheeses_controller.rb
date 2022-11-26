0489ae5f8f9ce13454ecb64e002990e2b042e0d9
martin mithi <gathitumithi9000@gmail.com>
2022-11-26 12:24:09 +0300

    commit tree 9a66584aa4c8633bd25eee4993708d0042659d8f

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,14 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    cheese=Cheese.find(params[:id])
+    render json: cheese
+  end
+
+  # def name
+  #   cheeses=Cheese.find_by(name: params["name"])
+  #   render json: cheeses
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get 'cheeses/:id', to: 'cheeses#show'
+  # get 'cheeses/:name', to: 'cheeses#name'
 end
